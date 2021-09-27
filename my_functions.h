3e6247d3aa3282241dd834b90d6e82b88a6e2ac6
Ritvars Puriņš <71127846+rpurins@users.noreply.github.com>
2021-09-27 19:19:23 +0200

    Create my_functions.h

---
+++ my_functions.h
@@ -0,0 +1,9 @@
+int str_length(char *mystring);
+void str_copy(char *source, char *destination);
+int str_find(char *needle, char *haystack);
+int get_unnamed_argument(int index, int argc, char **argv, char *result);
+int get_named_argument(int index, int argc, char **argv, char *result);
+
+void assertInt();
+void assertString();
+void clearStr();
