4cf43f29ef801f0a45697305157a5ece1d867607
Ritvars Puriņš <71127846+rpurins@users.noreply.github.com>
2021-09-30 15:05:43 +0200

    Update hw2.c

--- hw2.c
+++ hw2.c
@@ -1,1 +1,27 @@
-/* Enter Your code here */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "my_functions.h"
+#include "functions.h"
+#include "structure.h"
+
+int main(int argc, char const *argv[])
+{
+  int status = 0;
+  forest* data_structure = initForest(initTree(NULL));
+
+  status = read_persons(0, data_structure);
+  if (status != 0)
+  {
+    print_error(status); /* Just return in case of error */
+    return -1;
+  }
+  status = print_persons(1, data_structure);
+  if (status != 0)
+  {
+    print_error(status); /* Another error case */
+    return -1;
+  }
+
+  return 0;
+}
